7e099e8aa501bf30ca1b04e932e44df773ce46d4
aakash jaiswal <aakashjaiswal@hotmail.co.in>
2020-05-07 00:37:48 +0530

    Update may_6_.java

--- may_6_.java
+++ may_6_.java
@@ -1,1 +1,18 @@
-// hello world
+class Solution {
+    public int majorityElement(int[] nums) {
+        int maxIndex = 0;
+        int count = 1;
+        for(int i = 1 ; i< nums.length;i++){
+            if(nums[maxIndex]==nums[i]){
+                count ++;
+            }else{
+                count --;
+            }
+            if(count == 0){
+                count = 1;
+                maxIndex = i; 
+            }
+        }
+        return nums[maxIndex];
+    }
+}
