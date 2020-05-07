a5b6f67d6e14416c55f838df2fdae6511b0fa526
aakash jaiswal <aakashjaiswal@hotmail.co.in>
2020-05-08 03:03:45 +0530

    Update may_7_.java

--- may_7_.java
+++ may_7_.java
@@ -1,1 +1,46 @@
-// initial commit
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+     static int xDegree;
+     static int yDegree;
+    static int xParent;
+     static int yParent;
+    public boolean isCousins(TreeNode root, int x, int y) {
+      xDegree = -1;
+        yDegree = -1;
+        xParent=-1;
+        yParent=-1;
+        if(root == null)return false;
+        helper(root,0,-1,x,y);
+        if(xDegree == yDegree && xParent != yParent){
+            return true;
+        }
+        return false;
+    }
+    static void helper(TreeNode node,int degree,int parent,int x ,int y){
+       if(node == null)return;
+        if(node.val == x){
+            xDegree = degree;
+            xParent = parent;
+        }
+         if(node.val == y){
+            yDegree = degree;
+            yParent = parent;
+        }
+        helper(node.left,degree+1,node.val,x,y);
+         helper(node.right,degree+1,node.val,x,y);
+    }
+}
