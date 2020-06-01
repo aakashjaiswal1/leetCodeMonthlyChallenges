81771fd5e4d63195e352dca824c60eaa4e19af05
aakash jaiswal <aakashjaiswal@hotmail.co.in>
2020-06-01 17:51:06 +0530

    Update june_1_.java

--- june_1_.java
+++ june_1_.java
@@ -1,1 +1,29 @@
-//hello 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+    invert(root);
+        return root;
+    }
+    static void invert(TreeNode node){
+        if(node == null) return;
+        TreeNode temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+        invert(node.left);
+        invert(node.right);
+    }
+}
